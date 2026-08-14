ce6f1d78c14f81eae46304b3cfcbd4ea11ef006d
NAMB <gestionandreamartin@gmail.com>
2026-08-13 19:22:35 -0500

    añado comentario y segunda instrucción

--- proyecto.py
+++ proyecto.py
@@ -10,3 +10,6 @@ producto7 = float(input("Valor del producto 7: "))
 producto8 = float(input("Valor del producto 8: "))
 producto9 = float(input("Valor del producto 9: "))
 producto10 = float(input("Valor del producto 10: "))
+
+# Sumar los productos
+subtotal = (producto1 + producto2 + producto3 + producto4 + producto5 + producto6 + producto7 + producto8 + producto9 + producto10)
