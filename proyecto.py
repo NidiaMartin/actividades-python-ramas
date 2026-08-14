aae23f7de1915ce6f77a36afe76391b2ff8129e8
NAMB <gestionandreamartin@gmail.com>
2026-08-13 19:17:58 -0500

    añado comentario primera instruccion de factura

---
+++ proyecto.py
@@ -0,0 +1,12 @@
+# Factura de una papelería
+# Ingresar el valor de los 10 productos
+producto1 = float(input("Valor del producto 1: "))
+producto2 = float(input("Valor del producto 2: "))
+producto3 = float(input("Valor del producto 3: "))
+producto4 = float(input("Valor del producto 4: "))
+producto5 = float(input("Valor del producto 5: "))
+producto6 = float(input("Valor del producto 6: "))
+producto7 = float(input("Valor del producto 7: "))
+producto8 = float(input("Valor del producto 8: "))
+producto9 = float(input("Valor del producto 9: "))
+producto10 = float(input("Valor del producto 10: "))
